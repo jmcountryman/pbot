ae64d58c5595b79a4c4bd399b718f232be51c17c
Jamie Countryman <Jamie.Countryman@gmail.com>
2018-03-02 22:48:57 -0600

    Add voice welcome and logging

--- pbot.js
+++ pbot.js
@@ -1,8 +1,17 @@
 const config = require('./config');
 const Discord = require('discord.js');
 const moment = require('moment');
+const path = require('path');
 
 const client = new Discord.Client({disabledEvents: ['TYPING_START']});
+const welcomeClip = path.resolve(config.chris.welcomeClip);
+
+const log = function(message)
+{
+    const timestamp = moment();
+
+    console.log(`${timestamp} ${message}`);
+}
 
 const chipCount = function()
 {
@@ -23,25 +32,69 @@ const chipCount = function()
     });
 
     return name;
+};
+
+const isChris = function(user)
+{
+    return (user.id == config.chris.id);
 }
 
 client.on('ready', () =>
 {
-    console.log('Connected!');
+    log('Connected!');
+    log('Waiting for Chris...');
 });
 
-client.on('presenceChange', (oldMember, newMember) =>
+// when Chris logs on, update his nickname
+client.on('presenceUpdate', (oldMember, newMember) =>
 {
-    // Chris logged on
-    if (oldMember.id == config.chris.id &&
-        newMember.id == config.chris.id &&
+    log(`User presence change: ${newMember.id} - ${newMember.nickname}, ${oldMember.presence.status} => ${newMember.presence.status}`);
+
+    if (isChris(oldMember) &&
+        isChris(newMember) &&
         oldMember.presence.status == 'offline' &&
         newMember.presence.status == 'online')
     {
-        newMember.setNickname(chipCount());
+        log('Chris logged on!');
+        const newNickname = chipCount();
+        if(newMember.nickname !== newNickname)
+        {
+            log('Changing nickname.');
+            newMember.setNickname(chipCount());
+        }
     }
 });
 
-console.log(chipCount());
+// when Chris joins voice chat, welcome him
+client.on('voiceStateUpdate', (oldMember, newMember) =>
+{
+    log(`Voice status change: ${newMember.id} - ${newMember.nickname}`);
 
-// client.login(config.token);
+    const newChannel = newMember.voiceChannel;
+    
+    if (isChris(oldMember) &&
+        isChris(newMember) &&
+        oldMember.voiceChannel === undefined &&
+        newChannel !== undefined)
+    {
+        log('Chris joined a voice channel!');
+        log(`Joining ${newChannel.name}...`);
+
+        newChannel.join().then((connection) =>
+        {
+            log('Joined voice channel.');
+            log('Playing welcome message...');
+
+            const player = connection.playFile(welcomeClip);
+            player.on('end', () =>
+            {
+                log('Welcome message finished.');
+                log('Leaving voice channel.')
+                connection.disconnect();
+            });
+        });
+    }
+});
+
+// Let's do it!
+client.login(config.token);

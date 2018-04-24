cb00dadcdb2448fbf11aa4966d8ddad09c87b3c5
Jamie Countryman <Jamie.Countryman@gmail.com>
2018-04-23 19:18:15 -0500

    Improve redeploys

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,3 @@
 docker build --tag pissbot --build-arg config_file=config.prod.js .
-docker stop pissbot -t 10
+docker stop pissbot -t 10 && docker container rm pissbot
 docker run -itd --name pissbot pissbot

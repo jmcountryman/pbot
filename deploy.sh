9ee09095f01c7f3e56610406c5edb5f41abf7946
Jamie Countryman <Jamie.Countryman@gmail.com>
2018-04-28 15:20:04 -0500

    Add image cleanup to deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
+# Build a new image
 docker build --tag pissbot --build-arg config_file=config.prod.js .
+
+# Replace the running container (if any) with one based on the new image
 docker stop pissbot -t 10 && docker container rm pissbot
 docker run -itd --name pissbot pissbot
+
+# Remove old images to free up disk space
+docker images | grep none | awk '{ print $3 }' | xargs docker rmi
